004f0bdb049a81c664521e1c8a8d67b6254bc191
Michael Hitchens <mdhitchens@gmail.com>
2017-06-21 19:18:12 -0400

    Tweak .deb creator script

--- package.sh
+++ package.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -rf dist
+rm -f portal-gm-pre-slice.deb
 
 mkdir -p dist/usr/bin
 mkdir -p dist/usr/share/portal-gm-pre-slice
@@ -9,6 +10,7 @@ mkdir -p dist/usr/share/applications
 cp -r DEBIAN dist/
 
 cp portal-gm-pre-slice dist/usr/bin
+cp game dist/usr/share/portal-gm-pre-slice
 
 cp icon.png dist/usr/share/portal-gm-pre-slice
 cp portal-gm-pre-slice.desktop dist/usr/share/applications
